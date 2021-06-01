3767e153a8ae128d566a213eeb8b77323e7caac7
Stephen Moore <stephen@delfick.com>
2021-06-02 00:32:35 +1000

    [update] improve implementation and only support python3

--- venvstarter.py
+++ venvstarter.py
@@ -43,6 +43,7 @@ from distutils.version import StrictVersion
 from textwrap import dedent
 import subprocess
 import tempfile
+import shutil
 import shlex
 import json
 import sys
@@ -63,6 +64,105 @@ class memoized_property(object):
             setattr(instance, self.key, obj)
         return obj
 
+    def __set__(self, instance, value):
+        setattr(instance, self.key, value)
+
+
+class PythonFinder:
+    def __init__(self, min_python, max_python):
+        self.min_python = min_python
+        self.max_python = max_python
+
+    def suitable(self, version):
+        if version is None:
+            return False
+
+        if version < self.min_python:
+            return False
+
+        if self.max_python is not None and version > self.max_python:
+            return False
+
+        return True
+
+    def version_for(self, name):
+        executable = shutil.which(name)
+        if executable is None:
+            return None, None
+
+        question = [
+            executable,
+            "-c",
+            "import sys, json; print(json.dumps(list(sys.version_info)))",
+        ]
+
+        try:
+            version_info = subprocess.check_output(question).strip().decode()
+        except subprocess.CalledProcessError:
+            return executable, None
+        else:
+            return executable, StrictVersion("{0}.{1}.{2}".format(*json.loads(version_info)))
+
+    def versions(self, starting):
+        version = starting
+        while version < self.min_python:
+            if version.version[1] > self.min_python.version[1]:
+                version = StrictVersion(f"{version.version[0]+1}")
+            else:
+                version = StrictVersion(f"{version.version[0]+1}.{version.version[1]}")
+
+        while version >= self.min_python:
+            yield "python{0}.{1}".format(*version.version)
+
+            if version.version[1] == 0:
+                if version.version[0] == 0:
+                    break
+
+                version = StrictVersion(f"{version.version[0]-1}.{0}")
+            else:
+                version = StrictVersion(f"{version.version[0]}.{version.version[1]-1}")
+
+        version = starting
+        while version >= self.min_python:
+            yield "python{0}".format(*version.version)
+
+            if version.version[0] <= 3:
+                break
+
+            version = StrictVersion(f"{version.version[0] - 1}")
+
+    def find(self):
+        max_python = self.min_python
+        if self.max_python is None:
+            _, max_python_1 = self.version_for("python3")
+            _, max_python_2 = self.version_for("python")
+            found = [
+                m for m in (max_python_1, max_python_2) if m is not None and m > self.min_python
+            ]
+            if len(found) > 1:
+                max_python = max([max_python_1, max_python_2])
+            elif len(found) == 1:
+                max_python = found[0]
+        else:
+            max_python = self.max_python
+
+        tried = []
+        for version in self.versions(max_python):
+            tried.append(version)
+            executable, found = self.version_for(version)
+            if self.suitable(found):
+                return executable
+
+        raise Exception(
+            "\n".join(
+                [
+                    "\nCouldn't find a suitable python!",
+                    f"Wanted between {self.min_python} and {max_python}",
+                    f"Tried {', '.join(tried)}",
+                ]
+            )
+        )
+
 
 class Starter(object):
     """
@@ -76,9 +176,19 @@ class Starter(object):
         from your bootstrap script.
 
     program
-        The name of the program in the virtualenv's ``bin`` directory to invoke.
+        The program to run as None, a list, a string or as a function.
 
-        venvstarter will do a ``os.exec`` to this program as it's last action.
+        If set as None, then the python in the virtualenv is run
+
+        If the program is given as a string, we invoke it from the scripts in the
+        virtualenv.
+
+        If the program is given as a list, then we `os.execve(result[0], result + args, env)`
+
+        If the program is given as a function, that function is provided the location to
+        the python in the virtualenv. If the function returns `None` then venvstarter will
+        do nothing more. Otherwise if it will continue as if the program was the result of
+        the function all along.
 
     deps
         An optional list of pip dependencies to install into your virtualenv
@@ -91,10 +201,10 @@ class Starter(object):
         is the folder the virtualenv sits in.
 
     min_python_version
-        Either a string or a distutils.version.StrictVersion instance
+        An optional string or a distutils.version.StrictVersion instance
         representing the minimum version of python needed for the virtualenv.
 
-        This will always default to 3.5.
+        This will always default to 3.6.
 
     max_python_version
         An optional string or a distutils.version.StrictVersion instance
@@ -116,8 +226,9 @@ class Starter(object):
         program,
         deps=None,
         env=None,
-        min_python_version=3.5,
+        min_python_version=None,
         max_python_version=None,
+        venv_folder_name=None,
     ):
         self.env = env
         self.deps = deps
@@ -126,13 +237,20 @@ class Starter(object):
         self.min_python_version = min_python_version
         self.max_python_version = max_python_version
 
+        if venv_folder_name is None:
+            venv_folder_name = f".{self.program}"
+        self.venv_folder_name = venv_folder_name
+
         if self.max_python is not None and self.min_python > self.max_python:
             raise Exception("min_python_version must be less than max_python_version")
+
+        if self.min_python_version < 3:
+            raise Exception("Only support python3 and above")
 
     @memoized_property
     def min_python(self):
         if self.min_python_version is None:
-            return StrictVersion("3.5")
+            return StrictVersion("3.6")
         elif isinstance(self.min_python_version, StrictVersion):
             return self.min_python_version
         else:
@@ -156,88 +274,35 @@ class Starter(object):
         if not os.path.exists(folder):
             os.makedirs(folder)
 
-        return os.path.abspath(os.path.join(folder, ".{0}".format(self.program)))
+        return os.path.abspath(os.path.join(folder, self.venv_folder_name))
 
-    @memoized_property
-    def program_location(self):
-        if os.environ.get("MANAGED_VIRTUALENV", None) != "1":
-            return os.path.join(self.venv_location, "bin", self.program)
+    def venv_script(self, name):
+        if os.name == "nt":
+            return os.path.join(self.venv_location, "Scripts", name)
         else:
-            return subprocess.check_output(["which", self.program]).strip()
-
-    @memoized_property
-    def pip_location(self):
-        return os.path.join(self.venv_location, "bin", "pip")
-
-    @memoized_property
-    def activate_location(self):
-        return os.path.join(self.venv_location, "bin", "activate")
+            return os.path.join(self.venv_location, "bin", name)
 
     @memoized_property
     def venv_python(self):
-        return os.path.join(self.venv_location, "bin", "python")
-
-    @memoized_property
-    def venv_pip(self):
-        return os.path.join(self.venv_location, "bin", "pip")
-
-    @memoized_property
-    def python_location(self):
-        def suitable(version):
-            if version == self.min_python:
-                return True
-
-            if version < self.min_python:
-                return False
-
-            if self.max_python is None:
-                return True
-
-            if version > self.max_python:
-                return False
-
-        this_python = sys.executable
-        this_python_version = StrictVersion("{0}.{1}.{2}".format(*sys.version_info))
-        if suitable(this_python_version):
-            return this_python
-
-        question = """{0} -c 'import sys, json; print(json.dumps(list(sys.version_info)))'"""
-        version_info = (
-            lambda exe: subprocess.check_output(shlex.split(question.format(exe))).strip().decode()
-        )
-        version_question = lambda exe: StrictVersion(
-            "{0}.{1}.{2}".format(*json.loads(version_info(exe)))
-        )
-
-        def is_suitable_python(location):
-            try:
-                # I would use shutil.which but I still want to support python2
-                found_python = subprocess.check_output(["which", location]).strip().decode()
-            except subprocess.CalledProcessError:
-                return None
-
-            found_python_version = version_question(found_python)
-            if suitable(found_python_version):
-                return found_python
-
-        ret = is_suitable_python("python")
-        if ret:
-            return ret
-
-        ret = is_suitable_python("python{0}".format(*self.min_python.version))
-        if ret:
-            return ret
-
-        ret = is_suitable_python("python{0}.{1}".format(*self.min_python.version))
-        if ret:
-            return ret
-
-        raise Exception("Couldn't find a suitable python!")
+        return self.venv_script("python")
 
     def make_virtualenv(self):
         if not os.path.exists(self.venv_location):
+            python_exe = PythonFinder(self.min_python, self.max_python).find()
+            print("Creating virtualenv")
+            print(f"Destination: {self.venv_location}")
+            print(f"Using: {python_exe}")
+            print()
+
             res = os.system(
-                "virtualenv {0} -p {1}".format(self.venv_location, self.python_location)
+                " ".join(
+                    shlex.quote(s)
+                    for s in (
+                        python_exe,
+                        "-c",
+                        f'__import__("venv").create("{self.venv_location}", with_pip=True)',
+                    )
+                )
             )
             if res != 0:
                 raise Exception("Failed to make the virtualenv!")
@@ -275,8 +340,7 @@ class Starter(object):
                 )
             )
 
-            cmd = "{0} -c '{1}'".format(self.venv_python, question)
-            return subprocess.call(shlex.split(cmd), env=env)
+            return subprocess.call([self.venv_python, "-c", question])
 
         ret = check_deps()
         if ret != 0:
@@ -284,8 +348,8 @@ class Starter(object):
                 reqs.write("\n".join(str(dep) for dep in self.deps).encode("utf-8"))
                 reqs.flush()
 
-                cmd = "{0} install -r {1}".format(self.venv_pip, reqs.name)
-                ret = subprocess.call(shlex.split(cmd), env=env)
+                cmd = [self.venv_python, "-m", "pip", "install", "-r", reqs.name]
+                ret = subprocess.call(cmd, env=env)
 
                 if ret != 0:
                     raise SystemExit(1)
@@ -294,21 +358,40 @@ class Starter(object):
             if ret != 0:
                 raise Exception("Couldn't install the requirements")
 
+    def determine_command(self):
+        program = self.program
+        if callable(self.program):
+            program = self.program(self.venv_python)
+            if program is None:
+                return
+
+        if program is None:
+            return [self.venv_python]
+        elif isinstance(program, str):
+            return [self.venv_script(program)]
+        elif isinstance(program, list):
+            return program
+        else:
+            raise Exception(f"Not sure what to do with this program: {program}")
+
     def start_program(self, args):
         env = dict(os.environ)
-        venv_parent = os.path.dirname(self.venv_folder)
+        venv_parent = os.path.dirname(self.venv_location)
         if self.env is not None:
-            env.update(dict((k, v.format(venv_parent=venv_parent)) for k, v in self.env.items()))
+            env.update({k: v.format(venv_parent=venv_parent) for k, v in self.env.items()})
 
         # Fix a bug whereby the virtualenv has the wrong sys.executable
         if "__PYVENV_LAUNCHER__" in env:
             del env["__PYVENV_LAUNCHER__"]
 
+        cmd = self.determine_command()
+        if not cmd:
+            return
+
         try:
-            os.execve(self.venv_python, [self.venv_python, self.program_location] + args, env)
-        except OSError:
-            print("Sorry!!!! Couldn't find {0}".format(self.program_location))
-            raise SystemExit(1)
+            os.execve(cmd[0], cmd + args, env)
+        except OSError as error:
+            sys.exit(error)
 
     def ignite(self, args=None):
         """
@@ -319,11 +402,10 @@ class Starter(object):
         if args is None:
             args = sys.argv[1:]
 
-        if os.environ.get("MANAGED_VIRTUALENV", None) != "1":
-            made = self.make_virtualenv()
+        made = self.make_virtualenv()
 
-            if made or os.environ.get("VENV_STARTER_CHECK_DEPS", None) != "0":
-                self.install_deps()
+        if made or os.environ.get("VENV_STARTER_CHECK_DEPS", None) != "0":
+            self.install_deps()
 
         self.start_program(args)
 
